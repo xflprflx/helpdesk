aa177321ec53f742d42b30c1c53f15cfa1e1cda0
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-06-03 20:34:02 -0300

    endpoint findAll para Tecnico

--- src/main/java/com/xflprflx/helpdesk/services/TecnicoService.java
+++ src/main/java/com/xflprflx/helpdesk/services/TecnicoService.java
@@ -1,5 +1,6 @@
 package com.xflprflx.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,9 @@ public class TecnicoService {
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
+	}
+
+	public List<Tecnico> findAll() {
+		return repository.findAll();
 	}
 }
