4864c449cd3fc6896050b22579c126b6188bfa95
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-06-03 15:22:06 -0300

    anotations jpa para classes de dominio

--- src/main/java/com/xflprflx/helpdesk/domain/Cliente.java
+++ src/main/java/com/xflprflx/helpdesk/domain/Cliente.java
@@ -3,12 +3,21 @@ package com.xflprflx.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Cliente extends Pessoa {
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 
+import com.xflprflx.helpdesk.domain.enums.Perfil;
+
+@Entity
+public class Cliente extends Pessoa {
+	private static final long serialVersionUID = 1L;
+
+	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Cliente() {
 		super();
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
--- src/main/java/com/xflprflx/helpdesk/domain/Tecnico.java
+++ src/main/java/com/xflprflx/helpdesk/domain/Tecnico.java
@@ -3,12 +3,21 @@ package com.xflprflx.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.xflprflx.helpdesk.domain.enums.Perfil;
+
+@Entity
 public class Tecnico extends Pessoa {
+	private static final long serialVersionUID = 1L;
 	
+	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
 		super();
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
