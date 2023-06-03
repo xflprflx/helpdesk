303792fa47a6052d1b2e3e0128ac9fcde3a922af
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-06-03 19:53:15 -0300

    exceção para objeto não encontrado

--- src/main/java/com/xflprflx/helpdesk/services/TecnicoService.java
+++ src/main/java/com/xflprflx/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.xflprflx.helpdesk.domain.Tecnico;
 import com.xflprflx.helpdesk.repositories.TecnicoRepository;
+import com.xflprflx.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,6 +17,6 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
 	}
 }
