524dd13c0e3bd7941b8882d5223d98aff9250d05
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-06-04 23:43:20 -0300

    create pra chamado

--- src/main/java/com/xflprflx/helpdesk/services/ChamadoService.java
+++ src/main/java/com/xflprflx/helpdesk/services/ChamadoService.java
@@ -3,10 +3,17 @@ package com.xflprflx.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.xflprflx.helpdesk.domain.Chamado;
+import com.xflprflx.helpdesk.domain.Cliente;
+import com.xflprflx.helpdesk.domain.Tecnico;
+import com.xflprflx.helpdesk.domain.dtos.ChamadoDTO;
+import com.xflprflx.helpdesk.domain.enums.Prioridade;
+import com.xflprflx.helpdesk.domain.enums.Status;
 import com.xflprflx.helpdesk.repositories.ChamadoRepository;
 import com.xflprflx.helpdesk.services.exceptions.ObjectNotFoundException;
 
@@ -15,6 +22,10 @@ public class ChamadoService {
 
 	@Autowired
 	private ChamadoRepository repository;
+	@Autowired
+	private TecnicoService tecnicoService;
+	@Autowired
+	private ClienteService clienteService;
 	
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
@@ -24,8 +35,27 @@ public class ChamadoService {
 	public List<Chamado> findAll() {
 		return repository.findAll();
 	}
-	
 
+	public Chamado create(@Valid ChamadoDTO objDTO) {
+		return repository.save(newChamado(objDTO));
+	}
+	
+	private Chamado newChamado(ChamadoDTO obj) {
+		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+		Cliente cliente = clienteService.findById(obj.getCliente());
+		
+		Chamado chamado = new Chamado();
+		if(obj.getId() != null) {
+			chamado.setId(obj.getId());
+		}
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+		chamado.setStatus(Status.toEnum(obj.getStatus()));
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoes(obj.getObservacoes());
+		return chamado;
+	}
 	
 	
 	
