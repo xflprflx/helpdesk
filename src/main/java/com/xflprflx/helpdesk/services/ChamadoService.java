a0f84adab0b81c0c6865a72dc467f141c90dd74b
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-06-04 23:26:40 -0300

    findAll para chamado

--- src/main/java/com/xflprflx/helpdesk/services/ChamadoService.java
+++ src/main/java/com/xflprflx/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.xflprflx.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +20,12 @@ public class ChamadoService {
 		Optional<Chamado> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));	
 	}
+
+	public List<Chamado> findAll() {
+		return repository.findAll();
+	}
 	
+
 	
 	
 	
