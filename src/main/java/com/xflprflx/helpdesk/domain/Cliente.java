b0cdb2041102ed44a069d710a2a954fef5ee4cc8
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-06-04 22:48:25 -0300

    CRUD para cliente implementado

--- src/main/java/com/xflprflx/helpdesk/domain/Cliente.java
+++ src/main/java/com/xflprflx/helpdesk/domain/Cliente.java
@@ -2,11 +2,13 @@ package com.xflprflx.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.xflprflx.helpdesk.domain.dtos.ClienteDTO;
 import com.xflprflx.helpdesk.domain.enums.Perfil;
 
 @Entity
@@ -20,6 +22,17 @@ public class Cliente extends Pessoa {
 	public Cliente() {
 		super();
 		addPerfil(Perfil.CLIENTE);
+	}
+	
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
--- src/main/java/com/xflprflx/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/xflprflx/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.xflprflx.helpdesk.resources.exceptions;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolationException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -45,5 +46,19 @@ public class ResourceExceptionHandler {
 		}
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+	}
+	
+	@ExceptionHandler(ConstraintViolationException.class)
+	public ResponseEntity<StandardError> constraintViolationException(ConstraintViolationException ex, HttpServletRequest request) {
+	 
+	    StandardError error = new StandardError(
+	            System.currentTimeMillis(),
+	            HttpStatus.BAD_REQUEST.value(),
+	            "Validation Error",
+	            "Número do registro de contribuinte individual brasileiro (CPF) inválido!",
+	            request.getRequestURI()
+	    );
+	 
+	    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 }
